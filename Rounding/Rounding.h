e07714996dd82bb49af82fbbf12a3466d128e9bf
Christian Otkjær <christianotkjaer@icloud.com>
2017-11-05 17:31:09 +0100

    Initial Commit

---
+++ Rounding/Rounding.h
@@ -0,0 +1,19 @@
+//
+//  Rounding.h
+//  Rounding
+//
+//  Created by Christian Otkjær on 05/11/2017.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rounding.
+FOUNDATION_EXPORT double RoundingVersionNumber;
+
+//! Project version string for Rounding.
+FOUNDATION_EXPORT const unsigned char RoundingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rounding/PublicHeader.h>
+
+
